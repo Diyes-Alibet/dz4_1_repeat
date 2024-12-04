52830cfb562088503b26ed7121b276641b04f99d
Alibet <alibetasanov7config --global user.email>
2024-12-04 16:52:36 +0600

    new commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+
+function Header () {
+  return (
+      <h1>
+          <Title/>
+      </h1>
+  )
+}
+function Content () {
+  return(
+      <button>
+          <Title/>
+      </button>
+  )
+}
+function Footer () {
+    return (
+     <h2>
+         <Title/>
+     </h2>
+    )
+}
+function Title () {
+    return(
+        <span>
+      Diyes ALibet
+        </span>
+    )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
